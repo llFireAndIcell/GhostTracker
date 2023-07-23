918e65681931701fc3d75218fc16727d64a970c2
llFireAndIcell <llfireandicell@gmail.com>
2023-07-22 23:29:24 -0400

    Use one Gson in entire project instead of creating many

--- src/main/kotlin/me/fireandice/ghosttracker/utils/Utils.kt
+++ src/main/kotlin/me/fireandice/ghosttracker/utils/Utils.kt
@@ -2,7 +2,11 @@ package me.fireandice.ghosttracker.utils
 
 import cc.polyfrost.oneconfig.libs.universal.ChatColor
 import cc.polyfrost.oneconfig.libs.universal.UMinecraft
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 
 val FONT_HEIGHT by lazy { UMinecraft.getFontRenderer().FONT_HEIGHT }
+
+val gson: Gson = GsonBuilder().setPrettyPrinting().create()
 
 fun String.stripControlCodes(): String = ChatColor.stripControlCodes(this) ?: ""
