e871adc4ef4023a2dd0cca7256143e29dc3c0711
llFireAndIcell <llfireandicell@gmail.com>
2023-08-07 00:26:00 -0400

    Added support for hud scaling

--- src/main/kotlin/me/fireandice/ghosttracker/hud/elements/SuffixHudLine.kt
+++ src/main/kotlin/me/fireandice/ghosttracker/hud/elements/SuffixHudLine.kt
@@ -3,9 +3,9 @@ package me.fireandice.ghosttracker.hud.elements
 import cc.polyfrost.oneconfig.renderer.TextRenderer
 import cc.polyfrost.oneconfig.utils.dsl.mc
 import me.fireandice.ghosttracker.config.GhostConfig
-import me.fireandice.ghosttracker.hud.Image
 import me.fireandice.ghosttracker.utils.FONT_HEIGHT
-import net.minecraft.client.gui.Gui
+import me.fireandice.ghosttracker.utils.drawTexturedRect
+import net.minecraft.util.ResourceLocation
 import kotlin.reflect.KProperty0
 
 /**
@@ -22,7 +22,7 @@ class SuffixHudLine(
     var prefix: String? = null,
     var text: ColoredText,
     var margin: ColoredText,
-    private val image: Image,
+    private val image: ResourceLocation,
     private val visible: KProperty0<Boolean>,
     private val suffixVisible: () -> Boolean
 ) : HudLine {
@@ -41,20 +41,19 @@ class SuffixHudLine(
     override fun draw(x: Float, y: Float, scale: Float): Boolean {
         if (!visible.get()) return false
 
-        val scale = 1f  // TODO remove this when I add scaling support
-
         var currentX = x
         if (GhostConfig.showIcons) {
-            mc.textureManager.bindTexture(image.location)
-            Gui.drawModalRectWithCustomSizedTexture(
-                currentX.toInt(),
-                y.toInt(),
+            val guiScale = mc.gameSettings.guiScale
+            mc.textureManager.bindTexture(image)
+            drawTexturedRect(
+                currentX.toDouble(),
+                y.toDouble(),
                 0f,
                 0f,
-                8 * scale.toInt(),
-                8 * scale.toInt(),
-                image.width.toFloat(),
-                image.height.toFloat()
+                8 * scale.toDouble(),
+                8 * scale.toDouble(),
+                16f / guiScale * scale,
+                16f / guiScale * scale
             )
             currentX += 10 * scale
         }
--- src/main/kotlin/me/fireandice/ghosttracker/utils/Utils.kt
+++ src/main/kotlin/me/fireandice/ghosttracker/utils/Utils.kt
@@ -4,6 +4,8 @@ import cc.polyfrost.oneconfig.libs.universal.ChatColor
 import cc.polyfrost.oneconfig.libs.universal.UMinecraft
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import net.minecraft.client.renderer.Tessellator
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats
 
 val FONT_HEIGHT by lazy { UMinecraft.getFontRenderer().FONT_HEIGHT }
 
@@ -19,4 +21,23 @@ inline fun stringBuilder(block: StringBuilder.() -> Unit): String {
     val builder = StringBuilder()
     builder.block()
     return builder.toString()
+}
+
+/**
+ * An implementation similar to [Gui.drawModalRectWithCustomSizedTexture], but accepts [Double]s in place of [Int]s to
+ * allow more granularity when rendering
+ */
+fun drawTexturedRect(x: Double, y: Double, u: Float, v: Float, width: Double, height: Double, textureWidth: Float, textureHeight: Float) {
+    val f: Float = 1.0f / textureWidth
+    val g: Float = 1.0f / textureHeight
+    Tessellator.getInstance().apply {
+        worldRenderer.apply {
+            begin(7, DefaultVertexFormats.POSITION_TEX)
+            pos(x, y + height, 0.0).tex((u * f).toDouble(), ((v + height.toFloat()) * g).toDouble()).endVertex()
+            pos(x + width, y + height, 0.0).tex(((u + width.toFloat()) * f).toDouble(), ((v + height.toFloat()) * g).toDouble()).endVertex()
+            pos(x + width, y, 0.0).tex(((u + width.toFloat()) * f).toDouble(), (v * g).toDouble()).endVertex()
+            pos(x, y, 0.0).tex((u * f).toDouble(), (v * g).toDouble()).endVertex()
+        }
+        draw()
+    }
 }
