6c4ca802805ea366801bf388aec4fd6ffef1ae19
FireAndIce <llfireandicell@gmail.com>
2023-09-06 19:14:06 -0400

    Fixed images on other gui scales (auto still doesn't work)

--- src/main/kotlin/me/fireandice/ghosttracker/utils/Utils.kt
+++ src/main/kotlin/me/fireandice/ghosttracker/utils/Utils.kt
@@ -4,6 +4,7 @@ import cc.polyfrost.oneconfig.libs.universal.ChatColor
 import cc.polyfrost.oneconfig.libs.universal.UMinecraft
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import net.minecraft.client.gui.Gui
 import net.minecraft.client.renderer.Tessellator
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats
 
@@ -15,7 +16,7 @@ fun String.stripControlCodes(): String = ChatColor.stripControlCodes(this) ?: ""
 
 /**
  * Creates a new [StringBuilder], passes it as the block receiver, and returns it as a [String]
- * @return The result of [StringBuilder::toString()]
+ * @return The result of `StringBuilder::toString()`
  */
 inline fun stringBuilder(block: StringBuilder.() -> Unit): String {
     val builder = StringBuilder()
@@ -30,13 +31,26 @@ inline fun stringBuilder(block: StringBuilder.() -> Unit): String {
 fun drawTexturedRect(x: Double, y: Double, u: Float, v: Float, width: Double, height: Double, textureWidth: Float, textureHeight: Float) {
     val f: Float = 1.0f / textureWidth
     val g: Float = 1.0f / textureHeight
+
     Tessellator.getInstance().apply {
         worldRenderer.apply {
             begin(7, DefaultVertexFormats.POSITION_TEX)
-            pos(x, y + height, 0.0).tex((u * f).toDouble(), ((v + height.toFloat()) * g).toDouble()).endVertex()
-            pos(x + width, y + height, 0.0).tex(((u + width.toFloat()) * f).toDouble(), ((v + height.toFloat()) * g).toDouble()).endVertex()
-            pos(x + width, y, 0.0).tex(((u + width.toFloat()) * f).toDouble(), (v * g).toDouble()).endVertex()
-            pos(x, y, 0.0).tex((u * f).toDouble(), (v * g).toDouble()).endVertex()
+            // bottom left
+            pos(x, y + height, 0.0)
+                .tex((u * f).toDouble(), ((v + height.toFloat()) * g).toDouble())
+                .endVertex()
+            // bottom right
+            pos(x + width, y + height, 0.0)
+                .tex(((u + width.toFloat()) * f).toDouble(), ((v + height.toFloat()) * g).toDouble())
+                .endVertex()
+            // top right
+            pos(x + width, y, 0.0)
+                .tex(((u + width.toFloat()) * f).toDouble(), (v * g).toDouble())
+                .endVertex()
+            // top left
+            pos(x, y, 0.0)
+                .tex((u * f).toDouble(), (v * g).toDouble())
+                .endVertex()
         }
         draw()
     }
