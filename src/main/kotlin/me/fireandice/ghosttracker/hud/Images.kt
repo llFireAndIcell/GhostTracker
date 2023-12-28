b3d344d995c120346f2cfda10118c776ec437d52
DanceWithFire <llfireandicell@gmail.com>
2023-12-27 22:33:26 -0500

    Added texture for money hud

--- src/main/kotlin/me/fireandice/ghosttracker/hud/Images.kt
+++ src/main/kotlin/me/fireandice/ghosttracker/hud/Images.kt
@@ -10,8 +10,9 @@ object Images {
     val Volta = ResourceLocation(GhostTracker.MODID, "volta.png")
     val Plasma = ResourceLocation(GhostTracker.MODID, "plasma.png")
     val Boots = ResourceLocation(GhostTracker.MODID, "ghostly boots.png")
-    val Coins = ResourceLocation(GhostTracker.MODID, "coins.png")
+    val Coins = ResourceLocation(GhostTracker.MODID, "coin-drop.png")
     val MagicFind = ResourceLocation(GhostTracker.MODID, "magic find.png")
     val CombatXp = ResourceLocation(GhostTracker.MODID, "combat.png")
     val Time = ResourceLocation(GhostTracker.MODID, "time.png")
+    val Money = ResourceLocation(GhostTracker.MODID, "money.png")
 }
