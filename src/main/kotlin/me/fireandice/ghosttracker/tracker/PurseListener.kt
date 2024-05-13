8b24b9ed48c3631d19b6b9c694ddd34d33bfb042
DanceWithFire <llfireandicell@gmail.com>
2024-05-13 17:31:00 -0400

    Slight refactoring

--- src/main/kotlin/me/fireandice/ghosttracker/tracker/PurseListener.kt
+++ src/main/kotlin/me/fireandice/ghosttracker/tracker/PurseListener.kt
@@ -2,12 +2,13 @@ package me.fireandice.ghosttracker.tracker
 
 import me.fireandice.ghosttracker.GhostTracker
 import me.fireandice.ghosttracker.utils.ScoreboardUtils
+import java.util.Scanner
 
 object PurseListener {
 
     // closing paren can get cut off with enough digits. I think a 10b purse would break this but uhhh just use bank
-    private val PURSE_PATTERN = "(?<purse>[\\d,]+)( \\(\\+(?<scavenger>[\\d,]+)\\)?)?".toPattern()
-    private val COMBO_LOST_PATTERN = "Your Kill Combo has expired! You reached a [\\d,] Kill Combo!".toRegex()
+    private val PURSE_PATTERN = "(?<purse>\\d+)(?: \\(\\+(?<scavenger>\\d+)\\)?)?".toPattern()
+    private val COMBO_LOST_PATTERN = "Your Kill Combo has expired! You reached a [\\d,]+ Kill Combo!".toRegex()
     private var previousPurse = -1
     private var previousKills = -1
     private var previousScavenger = -1
@@ -15,25 +16,12 @@ object PurseListener {
     fun onTick() {
         val purseString = ScoreboardUtils.getPurse() ?: return
 
-        val matcher = PURSE_PATTERN.matcher(purseString)
-        if (!matcher.matches()) return
+        val scanner = Scanner(purseString)
 
         val kills = GhostTracker.ghostStats.kills
-        val purse: Int
-        val scavenger: Int
-
-        try {
-            purse = matcher.group("purse").toInt()
-            val scavengerMatch = matcher.group("scavenger")
-
-            scavenger =
-                if (previousScavenger == -1 && scavengerMatch == null) previousScavenger
-                else scavengerMatch?.toInt() ?: return
-
-        } catch (e: NumberFormatException) {
-            GhostTracker.logger.error("Couldn't parse purse or scavenger number")
-            return
-        }
+        val purse: Int = scanner.nextInt()
+        val scavenger: Int = if (scanner.hasNextInt()) scanner.nextInt()
+        else -1
 
         // if first tick after joining sb
         if (previousPurse == -1 || previousKills == -1) {
@@ -44,7 +32,7 @@ object PurseListener {
 
         val purseGained = purse - previousPurse
         val killsGained = kills - previousKills
-        if (purseGained > killsGained * scavenger) return // increase is too large; likely from another source
+        if (purseGained * 1.1 > killsGained * scavenger) return // increase is too large; likely from another source
 
         GhostTracker.ghostStats.scavenger += purseGained
         if (GhostTimer.isTracking) GhostTimer.stats.scavenger += purseGained
